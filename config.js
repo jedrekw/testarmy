efd988f99829ac6caa705164b4adcf9d324ea664
jedrekw <jedrekw@op.pl>
2017-11-17 14:41:46 +0100

    jenkins 17-11-2017c

--- config.js
+++ config.js
@@ -2,6 +2,10 @@ exports.config = {
     // seleniumAddress: 'http://localhost:4444/wd/hub',
     seleniumServerJar: './node_modules/seleniumserver.jar',
     specs: ['./specs/*.js'],
+    capabilities: {
+        'browserName': 'chrome',
+        'chromeOptions': {
+            'args': ['no-sandbox']
     baseUrl: "https://mytestarmy:!%40%23TestArmy@staging.testarmy.com",
     onPrepare: function() {
         jasmine.getEnv().addReporter(prettyReporter);
