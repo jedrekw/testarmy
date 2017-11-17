947695f51aeca6aa7723f6b9b70ffe2cce70016b
jedrekw <jedrekw@op.pl>
2017-11-17 17:04:16 +0100

    jenkins 17-11-2017f

--- configJenkins.js
+++ configJenkins.js
@@ -1,7 +1,7 @@
 exports.config = {
     // seleniumAddress: 'http://localhost:4444/wd/hub',
     seleniumServerJar: './node_modules/seleniumserver.jar',
-    chromeDriver: '/usr/lib/chromium-browser/chromedriver',
+    chromeDriver: './node_modules/chromedriver',
     specs: ['./specs/*.js'],
     baseUrl: "https://mytestarmy:!%40%23TestArmy@staging.testarmy.com",
     onPrepare: function() {
